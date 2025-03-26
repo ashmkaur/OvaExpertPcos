25476cf35fb37f561658729882748222e9ef97fa
ashmkaur <ashmkaur01@gmail.com>
2025-03-26 15:51:32 +0530

    Footer update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="bg-pink-200 py-4 mt-auto bottom-0 w-full">
             <div className="container mx-auto text-center">
-                <p>&copy; 2023 OvaExpert. All rights reserved.</p>
+                <p>&copy; 2025 OvaExpert. All rights reserved.</p>
             </div>
         </footer>
     );
